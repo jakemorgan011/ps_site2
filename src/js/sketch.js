b0f9cd2b751df551309b4d378e0ec493d77de3c6
jakemorgan <jakemorgan011703@gmail.com>
2025-06-16 18:46:28 -0700

    cube

---
+++ src/js/sketch.js
@@ -0,0 +1,22 @@
+function setup(){
+  createCanvas(500,500,WEBGL);
+  angleMode(DEGREES);
+  normalMaterial();
+}
+
+function draw(){
+  background(252, 252, 255);
+  // make box
+  push();
+  translate(0,0,0);
+  rotateWithFrameCount();
+  box(75,75,75); // could make some cool dynamic stuff here
+  // OR make some fancy vertex calculation stuff aswell.
+  pop();
+}
+
+function rotateWithFrameCount(){
+  rotateZ(frameCount);
+  rotateY(frameCount);
+  rotateX(frameCount);
+}
